4adca49359df4da95f8e03d88b6a10cfd09cefb6
elitan <johan@eliasson.me>
2018-11-29 09:45:37 +0100

    example starter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { ApolloProvider } from 'react-apollo';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import ApolloClient from 'apollo-client';
+// Setup the network "links"
+import { WebSocketLink } from 'apollo-link-ws';
+import { HttpLink } from 'apollo-link-http';
+import { split } from 'apollo-link';
+import { getMainDefinition } from 'apollo-utilities';
+
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// apollo client stuff
+const base_host = '' // TODO: Fill in host of Hasura server
+const wsurl = `ws://${base_host}/v1alpha1/graphql`;
+const httpurl = `http://${base_host}/v1alpha1/graphql`;
+
+const headers = {
+	'Content-Type': 'application/json',
+};
+
+const wsLink = new WebSocketLink({
+	uri: wsurl,
+	options: {
+		connectionParams: {
+			reconnect: true,
+			headers,
+		},
+	},
+});
+const httpLink = new HttpLink({
+	uri: httpurl,
+	headers,
+});
+
+const link = split(
+	// split based on operation type
+	({ query }) => {
+		const { kind, operation } = getMainDefinition(query);
+		return kind === 'OperationDefinition' && operation === 'subscription';
+	},
+	wsLink,
+	httpLink,
+);
+
+const client = new ApolloClient({
+	link,
+	cache: new InMemoryCache(),
+});
+
+ReactDOM.render(<ApolloProvider client={client}>
+	<App />
+</ApolloProvider>
+, document.getElementById('root'));
